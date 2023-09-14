840bbc088cbb464b6f62ef8cc84da7c3e6e94227
haoquachanh <anhhaomnmm@gmail.com>
2023-09-15 05:43:46 +0700

    user route

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,11 +1,29 @@
 import { Request, Response } from 'express';
+import { User } from '../entities/User.entities';
+import { Role } from '../entities/Role.entities';
+import { connectToDatabase } from '../connection';
 
 class UserController {
   async getAllUsers(req: Request, res: Response) {
     try {
-      
+      const connection = await connectToDatabase(); 
+
+      const userRepository = connection.getRepository(User);
+  
+      const users = await userRepository
+        .createQueryBuilder('user')
+        .leftJoinAndSelect('user.role', 'role')
+        .getMany()
+      res.status(200).json({
+        err: 0,
+        mes: users.length>0 ? "Got all users." : "No have any users.",
+        data: users
+      })
     } catch (error) {
-      res.status(500).json({ error: 'Internal server error' });
+      res.status(500).json({
+        err: -1,
+        mes: "Iternal Error",
+      });
     }
   }
 
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import userRouter from './user.route'; 
+import cardRouter from './card.route';
 
 const router = Router();
 
 router.use('/user', userRouter); 
+router.use('/card', cardRouter); 
 
 export default router;
