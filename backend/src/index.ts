00133da2fd08627070353102bb8a0c2ce294fb38
haoquachanh <anhhaomnmm@gmail.com>
2023-09-20 14:19:51 +0700

    print routes list

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata'
 import express from 'express';
 import router from './routes';
-import { dataSource } from './datasource';
+import { print } from './controllers/getActiveRoute';
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -9,9 +9,15 @@ app.use(express.json());
 
 app.use('/api', router)
 
-app.get('/', (req, res) => {
-  res.send('Server is running !');
-});
+app.get('/routes-list',(req,res)=>{
+  let list=[]
+  app._router.stack.forEach((layer) => {
+    const values = print([], layer);
+    list.push(...values);
+  })
+  res.status(200).json({list})
+})
+app.get('*', (req, res) => {res.send("SERVER IS RUNNING")})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
